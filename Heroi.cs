025ecb5e7d23ba3c64f20502f0b45d1be4d332ae
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2024-11-15 01:25:08 -0300

    Add project files.

---
+++ Heroi.cs
@@ -0,0 +1,37 @@
+﻿namespace EscrevendoClassesJogo
+{
+    public class Heroi
+    {
+        public Heroi(string nome, int idade, TipoHeroi tipoHeroi)
+        {
+            Nome = nome;
+            Idade = idade;
+            TipoHeroi = tipoHeroi;
+        }
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public TipoHeroi TipoHeroi { get; set; }
+
+        public void Atacar()
+        {
+            string ataque = TipoHeroi switch
+            {
+                TipoHeroi.Guerreiro => "espada",
+                TipoHeroi.Mago => "magia",
+                TipoHeroi.Monge => "artes marciais",
+                TipoHeroi.Ninja => "shuriken",
+                _ => "ataque",
+            };
+            Console.WriteLine($"O {this.TipoHeroi} atacou usando {ataque}.\n");
+        }
+    }
+
+    public enum TipoHeroi
+    {
+        Guerreiro = 1,
+        Mago = 2,
+        Monge = 3,
+        Ninja = 4
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EscrevendoClassesJogo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Digite o nome do herói: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite a idade do herói: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o tipo do herói: \n" +
+                    "1 - Guerreiro\n" +
+                    "2 - Mago\n" +
+                    "3 - Monge\n" +
+                    "4 - Ninja\n");
+                string enumHeroi = Console.ReadLine();
+                Enum.TryParse<TipoHeroi>(enumHeroi, out var enumParsed);
+
+                var heroi = new Heroi(nome, idade, enumParsed);
+                heroi.Atacar();
+
+                Console.WriteLine("Aperte qualquer botão para reiniciar o programa.");
+                Console.ReadKey();
+                Console.Clear();
+            } while (true);
+        }
+    }
+}
